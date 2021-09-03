b8f21c8e55b774227a0345cb1c5c9b4ac56c68fc
Zalmanp <tiagocarneiropimenta@gmail.com>
2021-09-03 11:29:07 -0300

    Exercises

---
+++ oo/Desafio 1.py
@@ -0,0 +1,9 @@
+num = input('Digite um número inteiro: ')
+if num.isdigit():
+    num = int(num)
+    if num % 2 == 0:
+        print(f"{num} é par. ")
+    else:
+        print(f"{num} é impar. ")
+else:
+    print(f"{num} não é um numero inteiro. ")
